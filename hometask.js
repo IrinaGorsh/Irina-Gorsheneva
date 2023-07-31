79fd5a8cef06abd6e47a6ce4c3ca7e1a146bc626
IrinaGorsh <129852222+IrinaGorsh@users.noreply.github.com>
2023-07-31 09:27:10 +0300

    Add files via upload

---
+++ hometask.js
@@ -0,0 +1,10 @@
+console.log("Текст из main.js!")
+//---------------------------------
+let tC = 16;
+let tF = (9/5) * tC + 32;
+console.log(`Температура по Цельсию = ${tC}`);
+console.log(`Температура по Фаренгейту = ${tF}`);
+//--------------------------------------
+let name = `Irina`;
+let admin = name;
+console.log(admin);
---
+++ script.js
@@ -0,0 +1,47 @@
+let num = 123;
+console.log(num);
+
+/*Комментарий 1*/
+let a = 5;
+a = 7;
+console.log(a);
+//Любой текст
+//Комментарий 2
+let b = 1+2+3
+console.log(b)
+//----------------------------------------------------
+const c = 10;
+const d = 2;
+let addition = c + d;
+let substraction = c - d;
+let multiplication = c * d;
+let division = c / d;
+console.log(`c + d = ${addition},  c - d = ${substraction}, c * d = ${multiplication}, c / d = ${division}`);
+console.log(`Сумма всех элементов = ${addition + substraction + multiplication + division}`);
+//-----------------------------------------------------
+
+const e = 1.5;
+const y = 0.75;
+let result = e + y;
+console.log(result);
+//-----------------------------------------------------
+
+const h = 5;
+console.log(`Число противоположнон числу h = ${h * -1}`);
+//-----------------------------------------------------
+
+const myName = `Irina`;
+const mySurname = `Gorsheneva`;
+console.log(`${myName} ${mySurname}`);
+//----------------------------------------------------
+
+let firstWord = `java`;
+let secondWord = `script`;
+
+console.log(firstWord + secondWord);
+//---------------------------------------------------
+
+let thirdWord = `hello`;
+let fourthWord = `world`;
+
+console.log(thirdWord + `` + fourthWord);
